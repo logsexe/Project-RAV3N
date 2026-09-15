48e7d4f205430e75d1dbac91e93c29b052315232
logsexe <thyloganallen@gmail.com>
2026-09-15 14:39:16 +1000

    Add FIELD OS core package

---
+++ fieldos/core/__init__.py
@@ -0,0 +1,5 @@
+"""Shared FIELD//OS runtime services."""
+
+from .hardware_service import HardwareService, HardwareSnapshot, UNKNOWN_TELEMETRY
+
+__all__ = ["HardwareService", "HardwareSnapshot", "UNKNOWN_TELEMETRY"]
